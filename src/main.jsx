b7921579f1ec2ae143d3d1ecaefbdc2e8c2ab9b8
bambang 785 <Adicandrabambang@gmail.com>
2023-07-24 19:07:47 +0700

    beta

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
