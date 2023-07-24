c22476f99a5f086a561145ee676d8ff14419fe06
bambang 785 <Adicandrabambang@gmail.com>
2023-07-24 19:05:00 +0700

    ver alpha

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Case from "./Case";
-import NavLink from "./Navlink";
+import NavLink from "./NavLink";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
