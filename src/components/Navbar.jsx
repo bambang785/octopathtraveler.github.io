e8d72e17ff6ed5d4c9e1f17836151cc39820aebf
bambang 785 <Adicandrabambang@gmail.com>
2023-07-29 16:23:51 +0700

    Merge branch 'main' of https://github.com/bambang785/octopathtraveler.github.io

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="bg-blue-600 py-2">
+    <div className="sticky top-0 z-50 bg-black py-3 opacity-60">
       <Case>
         <div className="flex items-center">
           <img src="../src/assets/OT1.png" alt="OT" className="w-50 h-16" />
           <Link
-            className=" mr-2 font-primary text-sm font-semibold uppercase text-white"
+            className=" mr-2 text-sm font-semibold uppercase text-white"
             to="/"
           ></Link>
           <NavLink href="octopathtraveler.github.io">Home</NavLink>
